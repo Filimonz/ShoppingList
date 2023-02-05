c77ce61672c976920730fc1da2d43605eee99238
Filimonz <yuriy.zhelannov@gmail.com>
2023-02-05 12:31:34 +0300

    Presentation слой. Часть 2

--- app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.shoppinglist.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.shoppinglist.data.ShopListRepositoryImpl
 import com.example.shoppinglist.domain.*
@@ -13,21 +12,14 @@ class MainViewModel: ViewModel() {
     private val editIShopItemUseCase = EditIShopItemUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
 
-    val shopList = MutableLiveData<List<ShopItem>>()
-
-    fun getShopList() {
-        val list = getShopListUseCase.getShopList()
-        shopList.value = list
-    }
+    val shopList = getShopListUseCase.getShopList()
 
     fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editIShopItemUseCase.editShopItem(newItem)
-        getShopList()
     }
 
     fun deleteShopItem(shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
-        getShopList()
     }
 }
