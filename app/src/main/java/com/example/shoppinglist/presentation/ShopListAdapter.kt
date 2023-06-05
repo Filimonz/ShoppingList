6ea410ad44e61f4f55c63120042c136215913667
Filimonz <yuriy.zhelannov@gmail.com>
2023-06-05 23:58:18 +0300

    Интеграция Data Binding в приложение ShoppingList

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -36,12 +36,10 @@ class ShopListAdapter: ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCal
 
         when (binding) {
             is ItemShopEnabledBinding -> {
-                binding.tvName.text = shopItem.name
-                binding.tvCount.text = shopItem.count.toString()
+                binding.shopItem = shopItem
             }
             is ItemShopDisabledBinding -> {
-                binding.tvName.text = shopItem.name
-                binding.tvCount.text = shopItem.count.toString()
+                binding.shopItem = shopItem
             }
         }
 
